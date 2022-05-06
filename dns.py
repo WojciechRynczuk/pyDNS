cc5d7a13761a020ff08750eee391e9c698092015
Malarz04 <krystian.gedlek@wp.pl>
2022-05-06 08:58:01 +0200

    video 5 complete

--- dns.py
+++ dns.py
@@ -1,10 +1,24 @@
 import socket
+
 port = 53
 ip = '127.0.0.1'
 
-sock = socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((ip, port))
+
+def buildresponse(data):
+
+    TransactionID = data[:2]
+    TID = ' '
+
+    for byte in TransactionID:
+        TID += (hex(byte))[2:]
+    print(TID)
+
+    #Flags = getflags(data[2:4])
+    return b''
 
 while 1:
     data, addr = sock.recvfrom(512)
-    print(data)
+    r = buildresponse(data)
+    sock.sendto(r, addr)
