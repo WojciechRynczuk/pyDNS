78a173d00556d0d51c740583241d24518e4e1490
Malarz04 <krystian.gedlek@wp.pl>
2022-05-12 07:39:44 +0200

    finish

--- dns.py
+++ dns.py
@@ -94,6 +94,41 @@ def getrecs(data):
 
     return (zone[qt], qt, domainname)
 
+def buildquestion(domainname, rectype):
+    qbytes = b''
+
+    for part in domainname:
+        length =len(part)
+        qbytes += bytes([length])
+
+        for char in  part:
+            qbytes += ord(char).to_bytes(1, byteorder='big')
+
+    if rectype == 'a':
+        qbytes += (2).to_bytes(1, byteorder=big)
+
+    qbytes += (2).to_bytes(1, byteorder='big')
+
+    return qbytes
+
+def rectobytes(domainname, rectype, recttl, recval):
+
+    rbytes = b'\xc0\x0c'
+
+    if rectype == 'a':
+        rbytes = rbytes + bytes([0]) + bytes([1])
+
+    rbytes = rbytes + bytes([0]) + bytes([1])
+
+    rbytes += int(recttl).to_bytes(4, byteorder='big')
+
+    if rectype == 'a':
+        rbytes = rbytes + bytes([0]) + bytes([4])
+
+        for part in recval.split('.'):
+            rbytes += bytes([int(part)])
+    return rbytes
+
 def buildresponse(data):
 
     TransactionID = data[:2]
@@ -111,7 +146,17 @@ def buildresponse(data):
 
     dnsheader = TransactionID+Flags+QDCOUNT+ANCOUNT+NSCOUNT+ARCOUNT
 
-    print(dnsheader)
+    dnsbody = b''
+
+    records, rectype, domainname = getrecs(data[12:])
+
+    dnsquestion = buildquestion(domainname, rectype)
+
+
+    for record in records:
+        dnsbody += rectobytes(domainname, rectype, record["ttl"], record["value"])
+
+    return dnsheader + dnsquestion + dnsbody
 
 while 1:
     data, addr = sock.recvfrom(512)
